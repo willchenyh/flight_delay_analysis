7d3d96f4ee276030361690eee08a7956eb30532e
Will Chen <willchenyh@gmail.com>
2017-12-04 00:14:57 -0800

    bokeh gui

--- will/bokeh_serve.py
+++ will/bokeh_serve.py
@@ -13,7 +13,7 @@ print y1*2
 
 # create the ColumnDataSource and pack the data in it
 m_lax = {i+1:ColumnDataSource(data=dict(t=t,y=y1*(i+1))) for i in range(12)}
-m_san = {i+1:ColumnDataSource(data=dict(t=t,y=y1*-i)) for i in range(12)}
+m_san = {i+1:ColumnDataSource(data=dict(t=t,y=y1*(i-12))) for i in range(12)}
 source_info = {'LAX':m_lax, 'SAN':m_san}
 
 # create the figure as usual
@@ -21,21 +21,25 @@ p = figure(width=300, height=200, tools="save,pan",
            x_axis_label='time (s)',y_axis_label='Amplitude')
 
 # add the line, but now using the ColumnDataSource
-line = p.line('t','y',source=source_info['LAX'][1],line_color='red')
+source = source_info['LAX'][1]
+line = p.line('t', 'y', source=source, line_color='red')
 
-menu = [("1 Hz", "1"),
-        ("5 Hz", "5"),
-        ("10 Hz", "10")]
+# menu = [("1 Hz", "1"),
+#         ("5 Hz", "5"),
+#         ("10 Hz", "10")]
 
 def cb(attr,old,new):
     ap = airport_select.value
     m = month_select.value
-    print m
+
     source = source_info[ap][m]
     d = dict(x=source.data['t'], y=source.data['y'])
+    print m, type(m), ap, type(ap)
+    print d
+
     source.data.update(d)
 
-airport_select = Select(value='LAX', title='Airports', options=['LAX','SAN'])
+airport_select = Select(value='LAX', title='Airport', options=['LAX','SAN'])
 month_select = Slider(start=1, end=12, value=1, step=1, title='Month')
 airport_select.on_change('value', cb)
 month_select.on_change('value', cb)
